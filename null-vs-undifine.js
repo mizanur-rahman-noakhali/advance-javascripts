0d36c943fc1988153851b2df51bc1383493f483c
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-04-11 16:34:39 +0600

    null-vs-undifine

--- null-vs-undifine.js
+++ null-vs-undifine.js
@@ -1,36 +1,34 @@
-//let paki;
-//console.log(paki);
+let paki;
+console.log(paki);
 
 
-// function add(num1,num2){
-//   console.log(num1+num2);
-// }                              no return
-// const result= add(65,75);
-// console.log(result);
+ function add(num1,num2){
+   console.log(num1+num2);
+ }                              //no return
+ const result= add(65,75);
+ console.log(result);
 
 
-// function add(num1,num2){
-//      console.log(num1+num2);
-//      return
-// }                              // return but no value
-//  const result= add(65,75);
-//  console.log(result);
+ function add(num1,num2){
+      console.log(num1+num2);
+      return
+ }                              // return but no value
+  const result= add(65,75);
+ console.log(result);
 
 
-// function add(num1,num2){
-//   console.log(num1,num2);
+ function add(num1,num2){
+   console.log(num1,num2);
   
-// }                              value no pass
-// const result= add(65);
-// console.log(result);
+ }                              //value no pass
+ const result= add(65);
+ console.log(result);
   
   
-// const premik={name:"abdul",phone:564789};  call deffrent objects
-// console.log(premik.gf);
+ const premik={name:"abdul",phone:564789}; // call deffrent objects console.log(premik.gf);
 
-  
-// let fun=undefined;
-// console.log(fun);
+   let fun=undefined;
+console.log(fun);
 
 
 let ages=[2,5,9]
