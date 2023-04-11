72df781f5856b31a4b9e75c7599bd5b4eb7dfbfe
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-04-11 17:52:19 +0600

    true-false

--- true-fulse.js
+++ true-fulse.js
@@ -1,6 +1,8 @@
 //const Name="0";
-                   //'0',' ', []>true
-let Name;         //0,null,undifine,NaN,false and emty string by defoult false 
+                   //'0',' ', []>
+                   
+let Name; //0,null,undifine,NaN,false and emty string by defoult false
+
                   // others true
 console.log(Name);
 if(Name){
