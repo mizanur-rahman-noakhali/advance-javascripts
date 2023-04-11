66ec97db39e9fb3b67b3f832c87ef7490cc627ef
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-04-11 16:28:04 +0600

    null vs undefine

---
+++ null-vs-undifine.js
@@ -0,0 +1,37 @@
+//let paki;
+//console.log(paki);
+
+
+// function add(num1,num2){
+//   console.log(num1+num2);
+// }                              no return
+// const result= add(65,75);
+// console.log(result);
+
+
+// function add(num1,num2){
+//      console.log(num1+num2);
+//      return
+// }                              // return but no value
+//  const result= add(65,75);
+//  console.log(result);
+
+
+// function add(num1,num2){
+//   console.log(num1,num2);
+  
+// }                              value no pass
+// const result= add(65);
+// console.log(result);
+  
+  
+// const premik={name:"abdul",phone:564789};  call deffrent objects
+// console.log(premik.gf);
+
+  
+// let fun=undefined;
+// console.log(fun);
+
+
+let ages=[2,5,9]
+console.log(ages[11]);
---
+++ true-fulse.js
@@ -0,0 +1,11 @@
+//const Name="0";
+                   //'0',' ', []>true
+let Name;         //0,null,undifine,NaN,false and emty string by defoult false 
+                  // others true
+console.log(Name);
+if(Name){
+  console.log("Condition is true");
+}
+else{
+ console.log("condition is false");
+}
