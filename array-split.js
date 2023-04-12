4bc75e06e1c30bae52e2a198def472a10927f43f
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-04-12 15:36:06 +0600

    advance javascripts

---
+++ array-split.js
@@ -0,0 +1,15 @@
+const nums = [1,2,3,4,5,6,7,8];
+//const part =nums.slice(2 ,5);
+
+//const remove=nums.splice(2,2);
+
+const remove=nums.splice(2,2,77,88);
+
+//console.log(part);
+
+console.log(nums);
+console.log(nums);
+
+const together=nums.join(",");
+console.log(together);
+
